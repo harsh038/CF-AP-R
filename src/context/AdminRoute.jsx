45e329a5bcf3333d2f1318137f94db67e4479729
Harsh Thummar <harshthummar2004@gmail.com>
2025-06-01 11:48:22 +0530

    done

--- src/context/AdminRoute.jsx
+++ src/context/AdminRoute.jsx
@@ -1,20 +1,16 @@
-import React from "react";
 import { Navigate, Outlet } from "react-router-dom";
 
 const AdminRoute = () => {
   const user = JSON.parse(localStorage.getItem("user"));
 
-  // Redirect to login if not authenticated
   if (!user) {
     return <Navigate to="/login" />;
   }
 
-  // Redirect to dashboard if not an admin
   if (user.role !== "Admin") {
     return <Navigate to="/dashboard" />;
   }
 
-  // Allow access to admin routes
   return <Outlet />;
 };
 
